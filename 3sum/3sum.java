35829fa26e252d7991f225b64046d452869df0bc
Deepak3493 <59064357+Deepak3493@users.noreply.github.com>
2023-07-07 19:37:49 +0530

    Time: 40 ms (80.88%) | Memory: 50.3 MB (85.24%) - LeetSync

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -1,33 +1,40 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        HashSet<List<Integer>> ansSet = new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            int k = nums[i];
-            getTwoSum(nums,i+1,ansSet,k);
-        }
         List<List<Integer>> ans = new ArrayList<>();
-        ans.addAll(ansSet);
+        for(int i=0;i<nums.length;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            int k = nums[i];
+            getTwoSum(nums,i+1,ans,k);
+        }
         return ans;
     }
     public static void getTwoSum(int nums[], 
-       int index, HashSet<List<Integer>> ansSet,int val){
+       int index, List<List<Integer>> ans,int val){
        int start = index;
        int end = nums.length-1;
        while(start<end){
            int first = nums[start];
            int second = nums[end];
            int sum = first+second+val;
-           if(sum==0){
-              if(!ansSet.contains(List.of(val, first, second))){
-                  ansSet.add(List.of(val, first, second));
-              }
-           }
            if(sum>0){
                end--;
            }
-           else{
-              start++;
+           else if(sum<0){
+               start++;
+           }else{
+                ans.add(List.of(val, first, second));
+                while((start+1<nums.length) && (nums[start+1] == nums[start])){
+                    start++;
+                }
+
+                while((end-1>=0) && (nums[end-1] == nums[end])){
+                    end--;
+                }
+                start++;
+                end--;
            }
        }
     }
