4321a216dd36e25715a11996d8c51fc3389dc9e4
Deepak3493 <59064357+Deepak3493@users.noreply.github.com>
2023-07-07 18:56:23 +0530

    Time: 2 ms (99.43%) | Memory: 43.4 MB (53.02%) - LeetSync

--- add-two-numbers/add-two-numbers.java
+++ add-two-numbers/add-two-numbers.java
@@ -14,14 +14,26 @@ class Solution {
         // ListNode head2 = reverse(l2);
         return add(l1,l2);
     }
-    public static ListNode reverse(ListNode head){
-        if(head == null || head.next == null)
-            return head;
-        ListNode newHead = reverse(head.next);
-        head.next.next = head;
-        head.next = null;
-        return newHead;
-    }
+    // public static ListNode reverse(ListNode head){
+    //     if(head == null || head.next == null)
+    //         return head;
+    //     ListNode newHead = reverse(head.next);
+    //     head.next.next = head;
+    //     head.next = null;
+    //     return newHead;
+    // }
+
+    // public static ListNode reverse(ListNode head){
+    //     ListNode pre = null;
+    //     ListNode curr = head;
+    //     while(curr!=null){
+    //         ListNode next = curr.next;
+    //         curr.next = prev;
+    //         prev = curr;
+    //         curr = next;
+    //     }
+    //     return prev;
+    // }
 
     public static ListNode add(ListNode head1, ListNode head2){
         if(head1==null)
