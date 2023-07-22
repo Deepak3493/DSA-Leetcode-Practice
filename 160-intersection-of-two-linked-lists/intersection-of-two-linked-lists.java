3a620ce21370f5cad5019ab980fe0d963fc4c3cd
Deepak3493 <59064357+Deepak3493@users.noreply.github.com>
2023-07-23 03:33:27 +0530

    Time: 1 ms (99.99%) | Memory: 46.7 MB (67.07%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -11,39 +11,42 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        int l1 = findLength(headA);
-        int l2 = findLength(headB);
-        int diff = l1-l2;
-        ListNode temp1 = null;
-        ListNode temp2 = null;
-        if(diff>0){
-           temp1 = headA;
-           temp2 = headB;
-        }
-        else{
-           temp1 = headB;
-           temp2 = headA;
-           diff = -diff;
-        }
-        while(diff!=0 && temp1!=null){
-           temp1=temp1.next;
-           diff--;
-        }
+        ListNode temp1 = headA;
+        ListNode temp2 = headB;
         while(temp1!=null && temp2!=null){
-            if(temp1.equals(temp2)){
-                return temp1;
-            }
             temp1 = temp1.next;
             temp2 = temp2.next;
         }
-        return null;
-    }
-    public int findLength(ListNode headA){
-        int count = 0;
-        while(headA!=null){
-            count++;
-            headA = headA.next;
+        if(headA == headB){
+            return headA;
         }
-        return count;
+        if(temp1==null){
+            temp1 = headB;
+            while(temp1!=null){
+                if(temp2 == null){
+                    temp2 = headA;
+                }
+                if(temp1 == temp2){
+                    return temp1;
+                }
+                temp1 = temp1.next;
+                temp2 = temp2.next;
+            }
+        }
+        if(temp2 == null){
+
+            temp2 = headA;
+            while(temp2!=null){
+                if(temp1 == null){
+                    temp1 = headB;
+                }
+                if(temp2 == temp1){
+                    return temp2;
+                }
+                temp2 = temp2.next;
+                temp1 = temp1.next;
+            }
+        }
+        return null;
     }
 }
