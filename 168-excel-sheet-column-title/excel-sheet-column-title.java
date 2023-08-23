f17c9f31b44fe394f5e9fdbcb25bb06df84427cc
Deepak3493 <59064357+Deepak3493@users.noreply.github.com>
2023-08-23 06:06:18 +0530

    Time: 6 ms (33.92%) | Memory: 40.5 MB (10.47%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.java
+++ 168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -2,8 +2,8 @@ class Solution {
     public String convertToTitle(int n) {
        String result="";
         while(n>0){
-            result=(char)('A'+(n-1)%26)+result;
-            n=(n-1)/26;
+            result=(char)('A'+(--n)%26)+result;
+            n=(n)/26;
         }
         return result;
     }
