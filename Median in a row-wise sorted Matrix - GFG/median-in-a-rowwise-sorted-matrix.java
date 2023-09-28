841d7be27e974af73ca58fc30f1a73812c844336
Deepak3493 <59064357+Deepak3493@users.noreply.github.com>
2023-09-29 03:06:25 +0530

    Added solution - LeetHub

--- Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.java
+++ Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.java
@@ -36,48 +36,69 @@ class GFG
 
 //User function Template for Java
 
+/*
+approach - 1
+
+ - place all the elements in a arraylist and sort all the elements and 
+ - return the element at (m*n)/2;
+ - space = m*n  time - m*nlog(m*n)+m*n;
+ 
+ 
+approach - 2
+ - find min and max from the whole matrix;
+ - now req element will be (min+max)/2;
+ - run the while loop between min and max as low and high
+ - find mid element by (low+high)/2;
+ -
+ 
+*/
 class Solution {
     int median(int matrix[][], int R, int C) {
-        // code here   
-        int low = Integer.MAX_VALUE;
-        int high = Integer.MIN_VALUE;
-        for(int i=0;i<R;i++){
-            low = Math.min(low,matrix[i][0]);
-            high = Math.max(high, matrix[i][C-1]);
-        }
-        
-        int reqMed = (R*C)/2;
-        while(low<=high){
-            int mid = (low+high)/2;
-            int countSmall = countSmaller(matrix, mid, R, C);
-            if(countSmall <= reqMed)
+       int min = Integer.MAX_VALUE;
+       int max = Integer.MIN_VALUE;
+       for(int i=0;i<R;i++){
+           for(int j=0;j<C;j++){
+               min = Math.min(matrix[i][j], min);
+               max = Math.max(matrix[i][j], max);
+           }
+       }
+       int req = (R*C)/2;
+       int low = min;
+       int high = max;
+       while(low<=high){
+           int mid = (low+high)/2;
+           int countOfReq = countReq(matrix,mid,R,C);
+           if(countOfReq<=req){
                low = mid+1;
-            else
+           }
+           else{
                high = mid-1;
-        }
-        return low-1;
+           }
+       }
+       return low;
     }
-    int countSmaller(int matrix[][], int mid, int row ,int col){
+    public static int countReq(int matrix[][], int mid, int R, int C){
         int count = 0;
-        for(int i=0;i<row;i++){
-            count+= upperBound(matrix[i], mid, col);
+        for(int i=0;i<R;i++){
+            count+=upperBound(matrix[i],mid,C);
         }
         return count;
     }
-    int upperBound(int matrix[], int req,int n){
+    public static int upperBound(int matrix[],int req, int n){
         int low = 0;
         int high = n-1;
-        int ans = n;
+        int bound = 0;
         while(low<=high){
             int mid = (low+high)/2;
-            if(matrix[mid]<req){
+            if(matrix[mid]<=req){
                 low = mid+1;
+                bound = low;
             }
             else{
-                ans = mid;
                 high = mid-1;
             }
         }
-        return ans;
+        return bound;
     }
+    
 }
