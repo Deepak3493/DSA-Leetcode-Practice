492eb446287192eaf51803d1e9f59bc4f457b664
Deepak3493 <59064357+Deepak3493@users.noreply.github.com>
2024-02-20 02:23:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (75.81%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,48 +1,46 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n = nums.length;
-        int fistSmallestInd = -1;
-        for(int i=n-1;i>0;i--){
+        int size = nums.length;
+        int index = -1;
+        for(int i = size-1;i>0;i--){
             if(nums[i]>nums[i-1]){
-                fistSmallestInd = i-1;
+                index = i-1;
                 break;
             }
         }
-        // System.out.println(fistSmallestInd);
-        if(fistSmallestInd==-1){
-            int start = 0;
-            int end = n-1;
-            while(start<=end){
-                int temp = nums[start];
-                nums[start] = nums[end];
-                nums[end] = temp;
-                start++;
-                end--;
+        if(index == -1){
+            int i = 0; int j = size-1;
+            while(i<j){
+                int start = nums[i];
+                nums[i] = nums[j];
+                nums[j] = start;
+                i++;
+                j--;
             }
-            return ;
+            return;
         }
-         
-        int indexToReplaceWith = -1;
-        for(int i=n-1;i>=0;i--){
-            if(nums[i]>nums[fistSmallestInd]){
-                indexToReplaceWith = i;
-                break;
+         //[1,2,3,3,2,4,5,6]
+        int k = size-1;
+        
+        while(k>=0  && k>index){
+            if(nums[k]>nums[index]){
+               break;
             }
+            k--;
         }
-        // System.out.println(indexToReplaceWith);
-        int temp = nums[indexToReplaceWith];
-        nums[indexToReplaceWith] = nums[fistSmallestInd];
-        nums[fistSmallestInd] = temp;
-
-
-        int start = fistSmallestInd+1;
-        int end = n-1;
-        while(start<=end){
-            int temp2 = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp2;
-            start++;
-            end--;
+        //reverse from k to size;
+        int start = nums[k];
+        nums[k] = nums[index];
+        nums[index] = start;
+        int m = nums.length-1;
+        int kk= index+1;
+        //System.out.println(index+" "+m);
+        while(kk<m){
+            start = nums[kk];
+            nums[kk] = nums[m];
+            nums[m] = start;
+            m--;
+            kk++;
         }
     }
 }
