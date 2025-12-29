76cbaa17f57c861be9e478966877cc76b77dd535
Deepak ojha <59064357+Deepak3493@users.noreply.github.com>
2025-12-30 04:19:31 +0530

    Time: 1 ms (44.6%), Space: 47.1 MB (10.46%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -4,7 +4,7 @@ class Solution {
         int columns = image[0].length;
         boolean visited[][] = new boolean[rows][columns];
         int startcolor = image[sr][sc];
-        dfs(sr, sc, visited, color, startcolor, image);
+        bfs(sr, sc, visited, color, startcolor, image);
         return image;
     }
     public static void dfs(int sr, int sc,boolean visited[][] ,  int color,int startcolor, int [][] image){
@@ -19,5 +19,28 @@ class Solution {
                 dfs(x,y,visited, color, startcolor, image);
             }
         }
+    }
+    public static void bfs(int sr, int sc,boolean visited[][] ,  int color,int startcolor, int [][] image){
+        int dx [] = {0,0,-1,1};
+        int dy [] = {-1,1,0,0};
+        Queue<int[]> q = new LinkedList<>();
+        int a [] = {sr,sc};
+        q.add(a);
+        while(q.size()>0){
+            a = q.poll();
+            sr = a[0];
+            sc = a[1];
+            image [sr][sc] = color;
+            visited[sr][sc] = true;
+            for(int i=0;i<4;i++){
+            int x = sr+dx[i];
+            int y = sc+dy[i];
+            if(x>=0 && y>=0 && x<image.length && y<image[0].length && visited[x][y] == false && image[x][y]== startcolor){
+                int ar [] = {x,y};
+                q.add(ar);
+            }
+           }
+        }
+
     }
 }
