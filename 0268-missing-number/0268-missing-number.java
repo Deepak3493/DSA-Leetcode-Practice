f13c14ed5eaaebcf82c9b8dd3ffa2cdffc11204a
Deepak ojha <59064357+Deepak3493@users.noreply.github.com>
2026-01-01 03:21:46 +0530

    Time: 0 ms (100%), Space: 47.2 MB (80.69%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,24 +1,30 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int i = 0;
-        while(i<nums.length){
-           int correctIndex = nums[i];
+        // int i = 0;
+        // while(i<nums.length){
+        //    int correctIndex = nums[i];
            
-           if(correctIndex<nums.length && nums[i]!=nums[correctIndex]){
-                int temp = nums[i];
-                nums[i] = nums[correctIndex];
-                nums[correctIndex] = temp;
-           }
-           else{
-                i++;
-           }
+        //    if(correctIndex<nums.length && nums[i]!=nums[correctIndex]){
+        //         int temp = nums[i];
+        //         nums[i] = nums[correctIndex];
+        //         nums[correctIndex] = temp;
+        //    }
+        //    else{
+        //         i++;
+        //    }
+        // }
+        // for(i=0;i<nums.length;i++){
+        //     if(nums[i]!=i){
+        //         return i;
+        //     }
+        // }
+        // return nums.length;
+        
+        //xor approach
+        int num = nums.length;
+        for(int i=0;i<nums.length;i++){
+            num = num^i^nums[i];
         }
-        for(i=0;i<nums.length;i++){
-            if(nums[i]!=i){
-                return i;
-            }
-        }
-        return nums.length;
-
+        return num;
     }
 }
