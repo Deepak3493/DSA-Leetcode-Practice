8571d760818aef7856873f48e1215dfa5a25db09
Deepak ojha <59064357+Deepak3493@users.noreply.github.com>
2026-01-03 10:33:17 +0530

    Time: 17 ms (2.3%), Space: 49.3 MB (5.1%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.java
+++ 0289-game-of-life/0289-game-of-life.java
@@ -2,34 +2,34 @@ class Solution {
     public void gameOfLife(int[][] board) {
        
         int newBoard[][] = new int[board.length][board[0].length];
-        int one = -1;
-        int zero = -2;
+        int initialOneTobeZero = -1;
+        int initialZeroTobeOne = -2;
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
                 int count = 0;
                 int k = board[i][j];
-                if(i>0 && (board[i-1][j]==1 || board[i-1][j]== one) ){
+                if(i>0 && (board[i-1][j]==1 || board[i-1][j]== initialOneTobeZero) ){
                     count++;
                 }
-                if(j>0 && (board[i][j-1]==1 ||  board[i][j-1] == one)){
+                if(j>0 && (board[i][j-1]==1 ||  board[i][j-1] == initialOneTobeZero)){
                     count++;
                 }
-                if(i+1<board.length && (board[i+1][j]==1|| board[i+1][j]==one )){
+                if(i+1<board.length && (board[i+1][j]==1|| board[i+1][j]==initialOneTobeZero )){
                     count++;
                 }
-                if(j+1<board[0].length && (board[i][j+1]==1 ||board[i][j+1]== one)){
+                if(j+1<board[0].length && (board[i][j+1]==1 ||board[i][j+1]== initialOneTobeZero)){
                     count++;
                 }
-                if(i+1<board.length && j+1<board[0].length && (board[i+1][j+1] == 1 ||  board[i+1][j+1] == one )){
+                if(i+1<board.length && j+1<board[0].length && (board[i+1][j+1] == 1 ||  board[i+1][j+1] == initialOneTobeZero )){
                     count++;
                 }
-                if(i-1>=0 && j-1>=0 && (board[i-1][j-1]==1 || board[i-1][j-1]== one) ){
+                if(i-1>=0 && j-1>=0 && (board[i-1][j-1]==1 || board[i-1][j-1]== initialOneTobeZero) ){
                     count++;
                 }
-                if(i>0 && j+1<board[0].length && (board[i-1][j+1]==1 ||  board[i-1][j+1]== one)){
+                if(i>0 && j+1<board[0].length && (board[i-1][j+1]==1 ||  board[i-1][j+1]== initialOneTobeZero)){
                     count++;
                 }
-                if(i+1<board.length && j-1>=0 && (board[i+1][j-1]==1 ||board[i+1][j-1]== one)){
+                if(i+1<board.length && j-1>=0 && (board[i+1][j-1]==1 ||board[i+1][j-1] == initialOneTobeZero)){
                     count++;
                 }
                 
@@ -39,21 +39,21 @@ class Solution {
                     board[i][j] = 1;
                 }
                 else if(k==0 && count ==3){
-                    board[i][j] = zero;
+                    board[i][j] = initialZeroTobeOne;
                 }
                 else {
                     if(board[i][j] == 1)
-                        board[i][j] = one;
+                        board[i][j] = initialOneTobeZero;
                 }
 
             }
         }
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
-                if(board[i][j] == zero){
+                if(board[i][j] == initialZeroTobeOne){
                     board[i][j] = 1;
                 }
-                if(board[i][j] == one){
+                if(board[i][j] == initialOneTobeZero){
                     board[i][j] = 0;
                 }
                 
