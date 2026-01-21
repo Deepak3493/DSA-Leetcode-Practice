52be047bb3029668fccc7ba60e20815d736ddea2
Deepak ojha <59064357+Deepak3493@users.noreply.github.com>
2026-01-21 09:19:01 +0530

    Time: 6 ms (1.63%), Space: 44 MB (34.53%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,50 +1,42 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int pivot = findMin(nums);
-        System.out.println(pivot);
-        if(pivot !=0 && target>=nums[0] && target<=nums[pivot-1]){
-            return find(nums, 0,pivot, target);
+        int start = 0;
+        int end = nums.length-1;
+        int pivot = 0;
+        while(start<end){
+            int mid = start+(end-start)/2;
+            int num = nums[mid];
+            if(nums[mid]>= nums[0] && nums[mid]<nums[mid+1]){
+                start = mid+1;
+                pivot = start;
+            }
+            else{
+                end = mid;
+            }
+        }
+        
+        if(target>=nums[0] && target<=nums[pivot]){
+            return binarySearch(nums, target, 0, pivot);
         }
         else{
-            return find(nums,pivot,nums.length-1, target);
+            return binarySearch(nums, target, pivot+1, nums.length-1);
         }
     }
-    public int find(int nums[], int start, int end, int target){
+    public static int binarySearch(int nums[], int target, int start ,int end){
+        System.out.println(start+" "+end);
         while(start<=end){
-            int mid = (start+end)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[mid]<target){
+            int mid = start+(end-start)/2;
+            int num = nums[mid];
+            if(num<target){
                 start = mid+1;
+            }
+            else if(num==target){
+                return mid;
             }
             else{
                 end = mid-1;
             }
         }
         return -1;
-    }
-
-     public int findMin(int[] nums) {
-        int start = 0;
-        int end = nums.length-1;
-        int startEle = nums[0];
-        int ans = startEle;
-        int ind = 0;
-        if(nums[0]<=nums[end])
-            return ans;
-        while(start<end){
-            int mid = (start+end)/2;
-            int midEle = nums[mid];
-            if(midEle>=startEle){
-                start = mid+1;
-                ans = nums[start];
-                ind = start;
-            }
-            else{
-                end = mid;
-            }
-        }
-        return ind;
     }
 }
