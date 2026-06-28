8f695851ee0ebf739edf8221522a82c84fbd1190
Deepak ojha <59064357+Deepak3493@users.noreply.github.com>
2026-06-28 20:47:30 +0530

    Time: 0 ms (100%), Space: 47.9 MB (45.17%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,30 +1,29 @@
 class Solution {
     public int trap(int[] height) {
-        int maxLeft = height[0];
-        int maxRight = height[0];
-        int indOfMaxRight = 0;
-        int ans = 0;
-        for(int i=1;i<height.length;i++){
-            if(maxRight<height[i]){
-               indOfMaxRight = i;
-               maxRight = height[i];
+        int size = height.length;
+        int left = 0;
+        int right = size-1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int total = 0;
+        while(left<right){
+            int leftBlock = height[left];
+            int rightBlock = height[right];
+            if(leftBlock<rightBlock){
+                if(leftMax > leftBlock){
+                   total += leftMax - leftBlock;
+                }
+                leftMax = Math.max(leftMax, leftBlock);
+                left++;
+            }
+            else{
+                if(rightMax > rightBlock){
+                   total += rightMax - rightBlock;
+                }
+                rightMax = Math.max(rightMax, rightBlock);
+                right--;
             }
         }
-        for(int i=1;i<indOfMaxRight;i++){
-           int k = height[i];
-           if(k<maxLeft && k<maxRight){
-              ans += (Math.min(maxRight,maxLeft) - k);
-           }
-           maxLeft = Math.max(maxLeft, k);
-        }
-        maxLeft = height[height.length-1];
-        for(int i=height.length-2;i>indOfMaxRight;i--){
-           int k = height[i];
-           if(k<maxLeft && k<maxRight){
-              ans += (Math.min(maxRight,maxLeft) - k);
-           }
-           maxLeft = Math.max(maxLeft, k);
-        }
-        return ans;
+        return total;
     }
 }
