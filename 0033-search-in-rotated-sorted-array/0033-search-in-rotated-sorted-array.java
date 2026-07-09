4bffd8449fc89a802e9d4581d06c157227f2dc90
Deepak ojha <59064357+Deepak3493@users.noreply.github.com>
2026-07-10 02:23:16 +0530

    Time: 0 ms (100%), Space: 44 MB (27.58%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,40 +1,39 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length-1;
-        int pivot = 0;
-        while(start<end){
-            int mid = start+(end-start)/2;
-            int num = nums[mid];
-            if(nums[mid]>= nums[0] && nums[mid]<nums[mid+1]){
-                start = mid+1;
-                pivot = start;
-            }
-            else{
-                end = mid;
-            }
-        }
-        
-        if(target>=nums[0] && target<=nums[pivot]){
-            return binarySearch(nums, target, 0, pivot);
+       int pivot =0;
+       int start =0;
+       int end = nums.length-1;
+       while(start<end){
+        int mid = (start+end)/2;
+        if(nums[mid]>=nums[0]){
+            start = mid+1;
+            pivot = mid;
         }
         else{
-            return binarySearch(nums, target, pivot+1, nums.length-1);
+            end = mid;
         }
+       }
+       //return pivot;
+
+       if(target >= nums[0] &&  target <= nums[pivot]){
+          return find(nums,0, pivot , target) ;
+        
+       }
+       else{
+          return find(nums, pivot+1, nums.length-1, target);
+       }
     }
-    public static int binarySearch(int nums[], int target, int start ,int end){
-        System.out.println(start+" "+end);
+    public static int find(int nums[], int start, int end, int target){
         while(start<=end){
-            int mid = start+(end-start)/2;
-            int num = nums[mid];
-            if(num<target){
-                start = mid+1;
-            }
-            else if(num==target){
+            int mid = (start+end)/2;
+            if(nums[mid]== target){
                 return mid;
             }
-            else{
+            else if(nums[mid]>target){
                 end = mid-1;
+            }
+            else{
+                start = mid+1;
             }
         }
         return -1;
