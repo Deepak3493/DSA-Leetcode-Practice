d9846e7c7e77da4f917ed69f74a3373ec319d471
Deepak ojha <59064357+Deepak3493@users.noreply.github.com>
2026-08-20 03:05:02 +0530

    Time: 0 ms (100%), Space: 43 MB (63.17%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,41 +1,33 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int count = 0;
-        int rows = matrix.length;
-        int columns = matrix[0].length;
-        int left = 0;
-        int top = 0;
-        int right = columns-1;
-        int bottom = rows-1;
         List<Integer> ans = new ArrayList<>();
-        while(count<rows*columns){
-            for(int i=left;i<=right && count<rows*columns;i++){
-                ans.add(matrix[top][i]);
-                count++;
-            }
-
-            top++;
-
-            for(int i=top;i<=bottom && count<rows*columns;i++){
-                ans.add(matrix[i][right]);
-                count++;
-            }
-
-            right--;
-
-            for(int j = right;j>=left && count<rows*columns;j--){
-                ans.add(matrix[bottom][j]);
-                count++;
-            }
-
-            bottom--;
-
-            for(int j = bottom;j>=top && count<rows*columns;j--){
-                ans.add(matrix[j][left]);
-                count++;
-            }
-
-            left++;
+        int top =0;
+        int bottom = matrix.length-1;
+        int left =0;
+        int right = matrix[0].length-1;
+        int total  = matrix.length* matrix[0].length;
+        int count =0;
+        while(count<total){
+           for(int i=left;i<=right && total>count;i++){
+                  count++;
+                  ans.add(matrix[top][i]);
+           }
+           top++;
+           for(int i=top;i<=bottom && total>count;i++){
+                  count++;
+                  ans.add(matrix[i][right]);
+           }
+           right--;
+           for(int i=right;i>=left && total>count;i--){
+                  count++;
+                  ans.add(matrix[bottom][i]);
+           }
+           bottom--;
+           for(int i=bottom;i>=top && total>count;i--){
+                  count++;
+                  ans.add(matrix[i][left]);
+           }
+           left++;
         }
         return ans;
     }
