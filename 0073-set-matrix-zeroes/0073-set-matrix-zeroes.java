c28225ed1a4432ee30add5cf231cfffb1a222721
Deepak ojha <59064357+Deepak3493@users.noreply.github.com>
2026-08-21 01:33:48 +0530

    Time: 1 ms (97.43%), Space: 47.7 MB (25.48%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,31 +1,45 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        boolean col0 = false;
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-
-                if(i==0){
-                    if (matrix[i][j] ==0){
-                        col0 = true;
-                    }
+        boolean row0 = false;
+        int n = matrix.length;
+        int m = matrix[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && matrix[i][j] == 0){
+                   row0 = true;
                 }
                 else{
-                    if(matrix[i][j]==0){
-                        matrix[i][0] = 0;
-                        matrix[0][j] = 0;
+                    if(matrix[i][j]==0 ){
+                        matrix[0][j]= 0;
+                        matrix[i][0]= 0;
                     }
+                }
+                
+            }
+        } 
+
+
+
+        //  for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //       System.out.print(matrix[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // } 
+
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(i==0){
+                    if(row0)
+                       matrix[i][j] = 0; 
+                }
+                else if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j] = 0;
+                    // System.out.println(i+" "+j);
                 }
             }
         }
-        for(int i=matrix.length-1;i>=0;i--){
-            for(int j = matrix[0].length-1;j>=0;j--){
-                    if(i == 0 && col0 == true){
-                        matrix[0][j]=0;
-                    }
-                    else if((matrix[i][0]==0 || matrix[0][j]==0) && i!=0){
-                        matrix[i][j]=0;
-                    }
-            }
-        }
+        
     }
 }
