7f2d615332818358ff2191594006f18235501e0d
Deepak ojha <59064357+Deepak3493@users.noreply.github.com>
2026-08-22 03:12:51 +0530

    Time: 4 ms (2.78%), Space: 47.7 MB (5.2%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0)
+            return true;
+        Boolean dp[][] = new Boolean[s.length()][t.length()];
+        return check(s, t, "", 0, 0, dp);
+    }
+
+    public boolean check(String s, String t, String ans, int i, int j, Boolean dp[][]) {
+        if (i >= s.length()) {
+            return true;
+        }
+        if (j >= t.length()) {
+            return false;
+        }
+
+        if (dp[i][j] != null)
+            return dp[i][j];
+
+        if (s.charAt(i) == t.charAt(j)) {
+            return dp[i][j] = check(s, t, ans + t.charAt(i), i + 1, j + 1, dp);
+        }
+
+        return dp[i][j] = check(s, t, ans, i, j + 1, dp);
+    }
+}
